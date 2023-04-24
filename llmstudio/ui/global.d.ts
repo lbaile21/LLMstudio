054f0588825a3687a9d9e147e96a819828901b90
Logan Bailey <logan.n.bailey@gmail.com>
2023-04-24 01:11:33 +0000

    Type dotlottie-player intrinsic element props

--- llmstudio/ui/global.d.ts
+++ llmstudio/ui/global.d.ts
@@ -1,5 +1,19 @@
 declare namespace JSX {
     interface IntrinsicElements {
-        "dotlottie-player": any;
+        "dotlottie-player": DotLottiePlayerAttributes;
     }
+}
+
+interface DotLottiePlayerAttributes {
+    src?: string;
+    autoplay?: boolean;
+    loop?: boolean;
+    speed?: number | string;
+    background?: string;
+    controls?: boolean;
+    direction?: 1 | -1;
+    mode?: "normal" | "bounce";
+    style?: React.CSSProperties;
+    className?: string;
+    children?: React.ReactNode;
 }
