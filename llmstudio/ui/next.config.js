035bc97d7c5a6ab7f2f60344b0627cce9ab839c7
Logan Bailey <logan.n.bailey@gmail.com>
2023-09-03 17:46:13 +0000

    Extract securityHeaders and add Frame/Permissions policies

--- llmstudio/ui/next.config.js
+++ llmstudio/ui/next.config.js
@@ -1,15 +1,20 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  { key: 'X-Content-Type-Options', value: 'nosniff' },
+  { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+  { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+  { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
+];
+
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  productionBrowserSourceMaps: false,
   async headers() {
     return [
       {
         source: '/:path*',
-        headers: [
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-        ],
+        headers: securityHeaders,
       },
     ];
   },
