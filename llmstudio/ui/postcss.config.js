540b940de94fe5d9c90292991340021de68599f3
Logan Bailey <logan.n.bailey@gmail.com>
2023-11-20 21:38:48 +0000

    Enable postcss-import, nesting, and prod cssnano minification

--- llmstudio/ui/postcss.config.js
+++ llmstudio/ui/postcss.config.js
@@ -1,7 +1,25 @@
 /** @type {import('postcss-load-config').Config} */
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+const isProduction = process.env.NODE_ENV === 'production';
+
+const plugins = {
+  'postcss-import': {},
+  'tailwindcss/nesting': {},
+  tailwindcss: {},
+  autoprefixer: {
+    flexbox: 'no-2009',
   },
+};
+
+if (isProduction) {
+  plugins.cssnano = {
+    preset: [
+      'default',
+      {
+        discardComments: { removeAll: true },
+        normalizeWhitespace: true,
+      },
+    ],
+  };
 }
+
+module.exports = { plugins };
